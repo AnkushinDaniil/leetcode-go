49b72c5b755b8a322254f8233ef6ca824506e17d
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-01-25 17:56:09 +0300

    RemoveDuplicatesFromSortedArray update

--- RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.go
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.go
@@ -1,21 +1,12 @@
 package removeDuplicatesFromSortedArray
 
 func RemoveDuplicates(nums []int) int {
-	n := len(nums)
-	if nums[0] == nums[n-1] {
-		return 1
-	}
-	i, j, num := 1, 0, nums[0]
-	for i+j < n {
-		for num == nums[i+j] {
-			j++
-			if i+j >= n {
-				return i
-			}
+	k := 1
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] != nums[i+1] {
+			nums[k] = nums[i+1]
+			k++
 		}
-		nums[i] = nums[i+j]
-		num = nums[i+j]
-		i++
 	}
-	return i
+	return k
 }
