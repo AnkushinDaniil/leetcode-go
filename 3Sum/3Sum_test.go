8e8dccd74f6f5fd4e03b30ce9432ba278042393b
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-03-15 12:59:58 +0300

    test uncommented

--- 3Sum/3Sum_test.go
+++ 3Sum/3Sum_test.go
@@ -12,14 +12,14 @@ func Test(t *testing.T) {
 		nums []int
 		want [][]int
 	}{
-		// {
-		// 	name: "Example 1",
-		// 	nums: []int{-1, 0, 1, 2, -1, -4},
-		// 	want: [][]int{
-		// 		{-1, -1, 2},
-		// 		{-1, 0, 1},
-		// 	},
-		// },
+		{
+			name: "Example 1",
+			nums: []int{-1, 0, 1, 2, -1, -4},
+			want: [][]int{
+				{-1, -1, 2},
+				{-1, 0, 1},
+			},
+		},
 		{
 			name: "Example 2",
 			nums: []int{0, 1, 1},
