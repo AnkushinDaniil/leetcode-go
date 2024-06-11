2e341992da96752e8cdb11a04a087223133e1d38
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-06-11 19:55:22 +0300

    binary search update

--- 1-DDynamicProgramming/LongestIncreasingSubsequence/LongestIncreasingSubsequence.go
+++ 1-DDynamicProgramming/LongestIncreasingSubsequence/LongestIncreasingSubsequence.go
@@ -1,6 +1,31 @@
 package longestIncreasingSubsequence
 
 func lengthOfLIS(nums []int) int {
+	arr := make([]int, 1, len(nums))
+	arr[0] = nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		l, r := 0, len(arr)
+		for l < r {
+			m := (l + r) / 2
+			if arr[m] < nums[i] {
+				l = m + 1
+			} else {
+				r = m
+			}
+		}
+
+		if l == len(arr) {
+			arr = append(arr, nums[i])
+		} else {
+			arr[l] = nums[i]
+		}
+	}
+
+	return len(arr)
+}
+
+func lengthOfLIS_DP(nums []int) int {
 	memo := make([]int, len(nums))
 	memo[len(nums)-1] = 1
 	res := 1
