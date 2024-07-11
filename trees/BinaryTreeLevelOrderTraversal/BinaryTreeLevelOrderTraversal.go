787239b3d468380c0309c24a4eb75080f7573ff2
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-11 22:20:47 +0300

    iter update

--- trees/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.go
+++ trees/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.go
@@ -5,24 +5,27 @@ import "github.com/AnkushinDaniil/leetcode-go/trees"
 type TreeNode = trees.TreeNode
 
 func levelOrder(root *TreeNode) [][]int {
-	res := make([][]int, 0)
-
-	var rec func(*TreeNode, int)
-	rec = func(tn *TreeNode, level int) {
-		if tn == nil {
-			return
-		}
-		if len(res) < level+1 {
-			res = append(res, []int{tn.Val})
-		} else {
-			res[level] = append(res[level], tn.Val)
-		}
-
-		rec(tn.Left, level+1)
-		rec(tn.Right, level+1)
+	if root == nil {
+		return [][]int{}
 	}
+	res := make([][]int, 0, 4)
+	queue := make([]*TreeNode, 0, 4)
+	queue = append(queue, root)
 
-	rec(root, 0)
+	for level := 0; len(queue) > 0; level++ {
+		res = append(res, make([]int, 0, len(queue)))
+		l := len(queue)
+		for range l {
+			res[level] = append(res[level], queue[0].Val)
+			if queue[0].Left != nil {
+				queue = append(queue, queue[0].Left)
+			}
+			if queue[0].Right != nil {
+				queue = append(queue, queue[0].Right)
+			}
+			queue = queue[1:]
+		}
+	}
 
 	return res
 }
