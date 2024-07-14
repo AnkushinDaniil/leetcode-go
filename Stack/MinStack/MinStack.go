b69acb138e5aafd39d634a255bc9b0a0d50f850a
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-14 08:24:16 +0300

    update

--- Stack/MinStack/MinStack.go
+++ Stack/MinStack/MinStack.go
@@ -1,56 +1,74 @@
 package minStack
 
-type Value struct {
-	minimum int
-	value   int
+// type MinStack struct {
+// 	stack, minStack []int
+// }
+
+// var (
+// 	arr1 [30000]int
+// 	arr2 [30000]int
+// )
+
+// func Constructor() MinStack {
+// 	return MinStack{
+// 		stack:    arr1[:0],
+// 		minStack: arr2[:0],
+// 	}
+// }
+
+// func (ms *MinStack) Push(val int) {
+// 	ms.stack = append(ms.stack, val)
+// 	if len(ms.minStack) == 0 {
+// 		ms.minStack = append(ms.minStack, val)
+// 	} else {
+// 		ms.minStack = append(ms.minStack, min(ms.GetMin(), val))
+// 	}
+// }
+
+// func (ms *MinStack) Pop() {
+// 	ms.stack = ms.stack[:len(ms.stack)-1]
+// 	ms.minStack = ms.minStack[:len(ms.minStack)-1]
+// }
+
+// func (ms *MinStack) Top() int {
+// 	return ms.stack[len(ms.stack)-1]
+// }
+
+// func (ms *MinStack) GetMin() int {
+// 	return ms.minStack[len(ms.minStack)-1]
+// }
+
+type Node struct {
+	val, minimum int
 }
 
-type MinStack struct {
-	stack []Value
-}
+type MinStack []Node
 
 func Constructor() MinStack {
-	return MinStack{
-		stack: []Value{},
-	}
+	return make(MinStack, 0, 16)
 }
 
-func (s *MinStack) Push(val int) {
-	value := Value{
+func (ms *MinStack) Push(val int) {
+	node := Node{
+		val:     val,
 		minimum: val,
-		value:   val,
 	}
 
-	if len(s.stack) == 0 {
-		s.stack = append(s.stack, value)
-		return
+	if len(*ms) > 0 {
+		node.minimum = min(val, ms.GetMin())
 	}
 
-	if val < s.stack[len(s.stack)-1].minimum {
-		value.minimum = val
-	} else {
-		value.minimum = s.stack[len(s.stack)-1].minimum
-	}
-	s.stack = append(s.stack, value)
+	*ms = append(*ms, node)
 }
 
-func (s *MinStack) Pop() {
-	s.stack = s.stack[:len(s.stack)-1]
+func (ms *MinStack) Pop() {
+	*ms = (*ms)[:len(*ms)-1]
 }
 
-func (s *MinStack) Top() int {
-	return s.stack[len(s.stack)-1].value
+func (ms *MinStack) Top() int {
+	return (*ms)[len(*ms)-1].val
 }
 
-func (s *MinStack) GetMin() int {
-	return s.stack[len(s.stack)-1].minimum
+func (ms *MinStack) GetMin() int {
+	return (*ms)[len(*ms)-1].minimum
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(val);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.GetMin();
- */
