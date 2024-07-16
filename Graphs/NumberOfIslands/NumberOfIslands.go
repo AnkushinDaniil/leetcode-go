7dae345d0250a9e3fffca10e3ee6b3911896783c
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-16 08:23:54 +0300

    update

--- Graphs/NumberOfIslands/NumberOfIslands.go
+++ Graphs/NumberOfIslands/NumberOfIslands.go
@@ -1,38 +1,37 @@
 package numberOfIslands
 
 func numIslands(grid [][]byte) int {
-	rows, cols := len(grid), len(grid[0])
+	m := len(grid)
+	n := len(grid[0])
+	queue := make([][2]int, 0, m*n)
 	islands := 0
-	q := make([][2]int, 0, rows*cols)
 
-	bfs := func(i, j int) {
-		grid[i][j] = '2'
-		q = q[:0]
-		q = append(q, [2]int{i, j})
-		for len(q) > 0 {
-			i, j := q[0][0], q[0][1]
-			q = q[1:]
-			if i+1 < rows && grid[i+1][j] == '1' {
-				q = append(q, [2]int{i + 1, j})
-				grid[i+1][j] = '2'
-			}
-			if j+1 < cols && grid[i][j+1] == '1' {
-				q = append(q, [2]int{i, j + 1})
-				grid[i][j+1] = '2'
-			}
-			if i-1 >= 0 && grid[i-1][j] == '1' {
-				q = append(q, [2]int{i - 1, j})
-				grid[i-1][j] = '2'
-			}
-			if j-1 >= 0 && grid[i][j-1] == '1' {
-				q = append(q, [2]int{i, j - 1})
-				grid[i][j-1] = '2'
-			}
+	updateIfOne := func(i, j int) {
+		if i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == '1' {
+			queue = append(queue, [2]int{i, j})
+			grid[i][j] = '2'
 		}
 	}
 
-	for row := 0; row < rows; row++ {
-		for col := 0; col < cols; col++ {
+	checkAndUpdate := func(i, j int) {
+		updateIfOne(i+1, j)
+		updateIfOne(i, j+1)
+		updateIfOne(i-1, j)
+		updateIfOne(i, j-1)
+	}
+
+	bfs := func(i, j int) {
+		grid[i][j] = '2'
+		queue = append(queue, [2]int{i, j})
+		for len(queue) > 0 {
+			i, j = queue[0][0], queue[0][1]
+			queue = queue[1:]
+			checkAndUpdate(i, j)
+		}
+	}
+
+	for row := range m {
+		for col := range n {
 			if grid[row][col] == '1' {
 				bfs(row, col)
 				islands++
