34bb829efdb39c35657a609fd3b55b0eb67758ca
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-17 12:10:36 +0300

    refactored

--- Graphs/RottingOranges/RottingOranges.go
+++ Graphs/RottingOranges/RottingOranges.go
@@ -5,8 +5,8 @@ func orangesRotting(grid [][]int) int {
 	time, counter := 0, 0
 	queue := make([][2]int, 0, 4*max(rows, cols))
 
-	for row := 0; row < rows; row++ {
-		for col := 0; col < cols; col++ {
+	for row := range rows {
+		for col := range cols {
 			if grid[row][col] == 2 {
 				queue = append(queue, [2]int{row, col})
 			} else if grid[row][col] == 1 {
@@ -19,34 +19,32 @@ func orangesRotting(grid [][]int) int {
 		return 0
 	}
 
+	checkAndUpdate := func(i, j int) {
+		if i >= 0 && i < rows && j >= 0 && j < cols &&
+			grid[i][j] == 1 {
+			queue = append(queue, [2]int{i, j})
+			grid[i][j] = 2
+			counter--
+		}
+	}
+
+	checkAndUpdateNeighbors := func(i, j int) {
+		checkAndUpdate(i+1, j)
+		checkAndUpdate(i, j+1)
+		checkAndUpdate(i-1, j)
+		checkAndUpdate(i, j-1)
+	}
+
 	for len(queue) > 0 {
 		lenQueue := len(queue)
 		time++
 		for i := 0; i < lenQueue; i++ {
 			row, col := queue[0][0], queue[0][1]
 			queue = queue[1:]
-			if row+1 < rows && grid[row+1][col] == 1 {
-				queue = append(queue, [2]int{row + 1, col})
-				grid[row+1][col] = 2
-				counter--
-			}
-			if col+1 < cols && grid[row][col+1] == 1 {
-				queue = append(queue, [2]int{row, col + 1})
-				grid[row][col+1] = 2
-				counter--
-			}
-			if row-1 >= 0 && grid[row-1][col] == 1 {
-				queue = append(queue, [2]int{row - 1, col})
-				grid[row-1][col] = 2
-				counter--
-			}
-			if col-1 >= 0 && grid[row][col-1] == 1 {
-				queue = append(queue, [2]int{row, col - 1})
-				grid[row][col-1] = 2
-				counter--
-			}
+			checkAndUpdateNeighbors(row, col)
 		}
 	}
+
 	if counter == 0 {
 		return time - 1
 	} else {
