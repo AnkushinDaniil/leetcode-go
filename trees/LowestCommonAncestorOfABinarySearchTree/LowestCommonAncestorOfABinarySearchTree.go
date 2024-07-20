ed50ecbff1844c2566ac167d740bb42f1226bd05
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-20 14:24:17 +0300

    refactored

--- trees/LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.go
+++ trees/LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.go
@@ -5,26 +5,22 @@ import "github.com/AnkushinDaniil/leetcode-go/trees"
 type TreeNode = trees.TreeNode
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	l, r := p.Val, q.Val
 	var res *TreeNode
 
 	var dfs func(*TreeNode) bool
 	dfs = func(tn *TreeNode) bool {
-		if res != nil {
+		if res != nil || tn == nil {
 			return false
 		}
-		if tn == nil {
-			return false
-		}
-		t := false
-		if tn.Val == l || tn.Val == r {
-			t = true
-		}
+
+		t := tn.Val == p.Val || tn.Val == q.Val
 		l := dfs(tn.Left)
 		r := dfs(tn.Right)
+
 		if (t && l) || (t && r) || (l && r) {
 			res = tn
 		}
+
 		return t || l || r
 	}
 	dfs(root)
